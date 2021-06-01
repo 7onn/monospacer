19d1d392d9d5561cc3ee85db896dcfd2d141ef69
tompsh <devbytom@gmail.com>
2021-06-01 20:59:14 -0300

    Fibonacci toggleFontFamily

--- monospacer.js
+++ monospacer.js
@@ -4,7 +4,18 @@ toggleFontFamily = () => {
   for (i = 0; i < all.length; i++) {
     all[i].style.fontFamily = "Monospace, monospace, Menlo, menlo"
   }
-  setTimeout(toggleFontFamily, 2000)
 }
 
 toggleFontFamily()
+setTimeout(toggleFontFamily, 100)
+setTimeout(toggleFontFamily, 200)
+setTimeout(toggleFontFamily, 300)
+setTimeout(toggleFontFamily, 500)
+setTimeout(toggleFontFamily, 800)
+setTimeout(toggleFontFamily, 1300)
+setTimeout(toggleFontFamily, 2100)
+setTimeout(toggleFontFamily, 3400)
+setTimeout(toggleFontFamily, 5500)
+setTimeout(toggleFontFamily, 8900)
+setTimeout(toggleFontFamily, 14400)
+setTimeout(toggleFontFamily, 23300)

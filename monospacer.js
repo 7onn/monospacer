2aab79a60b4612f6a12864f90b86d59095fe90ac
tompsh <devbytom@gmail.com>
2021-06-01 21:12:16 -0300

    Reload elements within toggleFontFamily

--- monospacer.js
+++ monospacer.js
@@ -1,6 +1,5 @@
-all = document.getElementsByTagName("*")
-
 toggleFontFamily = () => {
+  all = document.getElementsByTagName("*")
   for (i = 0; i < all.length; i++) {
     all[i].style.fontFamily = "Monospace, monospace, Menlo, menlo"
   }
